034ca33e8ab5191d38c8f2bd026dc71a2691d6d3
yakraj <princeyakraj289@gmail.com>
2025-03-01 10:07:05 +0530

    first commit

---
+++ sample.py
@@ -0,0 +1,21 @@
+import torch
+import torchaudio
+from zonos.model import Zonos
+from zonos.conditioning import make_cond_dict
+from zonos.utils import DEFAULT_DEVICE as device
+
+# model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-hybrid", device=device)
+model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-transformer", device=device)
+
+wav, sampling_rate = torchaudio.load("assets/exampleaudio.mp3")
+speaker = model.make_speaker_embedding(wav, sampling_rate)
+
+torch.manual_seed(421)
+
+cond_dict = make_cond_dict(text="Hello, world!", speaker=speaker, language="en-us")
+conditioning = model.prepare_conditioning(cond_dict)
+
+codes = model.generate(conditioning)
+
+wavs = model.autoencoder.decode(codes).cpu()
+torchaudio.save("sample.wav", wavs[0], model.autoencoder.sampling_rate)
